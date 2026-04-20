c03cddb8a562af60cd91ef1827f330c6a77bc194
xzael24 <zaimel2007@gmail.com>
2026-04-20 11:39:48 +0700

    first commit

---
+++ Readme.md
@@ -0,0 +1,6 @@
+Project Pemrograman Komputer
+
+List Anggota:
+1. Muhammad Zaim El Yafi
+2. Gusti Rizqi Putra Hanif
+3. Mohamad Naufal Arizal
---
+++ src/main/java/com/mycompany/sesuaitugas/SesuaiTugas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sesuaitugas;
+
+/**
+ *
+ * @author AcerAG14
+ */
+public class SesuaiTugas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
